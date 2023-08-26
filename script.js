d011b1c70c6acb78234d9b52e66a845799420429
Kareena <kareena22@navgurukul.org>
2023-08-26 23:45:44 +0530

    +1

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let currentPrice = document.querySelector("#current-price");
 let submitBtn = document.querySelector("#submit-btn");
 let outputBox = document.querySelector("#output-box");
 
-
-
 submitBtn.addEventListener("click", submitHandler)
 function submitHandler() {
     let ip = Number(initialPrice.value);
@@ -16,26 +14,29 @@ function submitHandler() {
     calculateProfitAndLoss(ip, qty, curr)
 }
 
-
-
-
 function calculateProfitAndLoss(initial, quantity, current) {
-    if (initial > current) {
+
+    if (initial == "" || quantity == "" || current == "") {
+        empty(`Enter Input is Must `);
+    }
+
+    else if (initial > current) {
         let loss = (initial - current) * quantity;
         let lossPercentage = (loss / initial) * 100;
         let lp = lossPercentage.toFixed(2);
 
-        showOutput(`The Loss is${loss} and the Loss Percentage is ${lp}%`);
+        showOutput(`The Loss is ${loss} and the Loss Percentage is ${lp}%`, "red");
     }
     else if (initial < current) {
         let profit = (current - initial) * quantity;
         let profitPercentage = (profit / initial) * 100;
         let pp = profitPercentage.toFixed(2);
 
-        showOutput(`The Profit is${profit} and the Profit Percentage is ${pp}`);
+        showOutput(`The Profit is ${profit} and the Profit Percentage is ${pp}`, "green");
     }
+
     else {
-        showOutput("No Profit No Loss");
+        showOutput("No Profit No Loss", "blue");
     }
 
 }
@@ -45,6 +46,11 @@ function calculateProfitAndLoss(initial, quantity, current) {
 // calculateProfitAndLoss(10, 10, 10)
 
 
-function showOutput(message) {
+function showOutput(message,color) {
     outputBox.innerHTML = message;
+    outputBox.style.color = color 
+}
+
+function empty(msg) {
+    outputBox.innerHTML = msg;
 }

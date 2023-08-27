cb1a1622e90d2a12796fec7b4153ae7b9e3693e2
Kareena <kareena22@navgurukul.org>
2023-08-27 20:40:49 +0530

    +1

--- script.js
+++ script.js
@@ -7,80 +7,80 @@ let outputBox = document.querySelector("#output-box");
 
 submitBtn.addEventListener("click", submitHandler)
 function submitHandler() {
-    let ip = Number(initialPrice.value);
-    let qty = Number(stockQuantity.value);
-    let curr = Number(currentPrice.value);
+  let ip = Number(initialPrice.value);
+  let qty = Number(stockQuantity.value);
+  let curr = Number(currentPrice.value);
 
-    calculateProfitAndLoss(ip, qty, curr)
+  calculateProfitAndLoss(ip, qty, curr)
 }
 
 function calculateProfitAndLoss(initial, quantity, current) {
 
-    if (initial == "" || quantity == "" || current == "") {
-        empty(`Enter Input is Must `);
+  if (initial == "" || quantity == "" || current == "") {
+    empty(`Enter Input is Must `);
+  }
+
+  else if (initial > current) {
+    let loss = (initial - current) * quantity;
+    let lossPercentage = (loss / initial) * 100;
+    let lp = lossPercentage.toFixed(2);
+
+    showOutput(`The Loss is ${loss} and the Loss Percentage is ${lp}%`, "red");
+  }
+  else if (initial < current) {
+    let profit = (current - initial) * quantity;
+    let profitPercentage = (profit / initial) * 100;
+    let pp = profitPercentage.toFixed(2);
+
+    showOutput(`The Profit is ${profit} and the Profit Percentage is ${pp}`, "green");
+
+
+
+
+    const count = 200,
+      defaults = {
+        origin: { y: 0.7 },
+      };
+
+    function fire(particleRatio, opts) {
+      confetti(
+        Object.assign({}, defaults, opts, {
+          particleCount: Math.floor(count * particleRatio),
+        })
+      );
     }
 
-    else if (initial > current) {
-        let loss = (initial - current) * quantity;
-        let lossPercentage = (loss / initial) * 100;
-        let lp = lossPercentage.toFixed(2);
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
 
-        showOutput(`The Loss is ${loss} and the Loss Percentage is ${lp}%`, "red");
-    }
-    else if (initial < current) {
-        let profit = (current - initial) * quantity;
-        let profitPercentage = (profit / initial) * 100;
-        let pp = profitPercentage.toFixed(2);
+    fire(0.2, {
+      spread: 60,
+    });
 
-        showOutput(`The Profit is ${profit} and the Profit Percentage is ${pp}`, "green");
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
 
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
 
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }
 
-
-        const count = 200,
-  defaults = {
-    origin: { y: 0.7 },
-  };
-
-function fire(particleRatio, opts) {
-  confetti(
-    Object.assign({}, defaults, opts, {
-      particleCount: Math.floor(count * particleRatio),
-    })
-  );
-}
-
-fire(0.25, {
-  spread: 26,
-  startVelocity: 55,
-});
-
-fire(0.2, {
-  spread: 60,
-});
-
-fire(0.35, {
-  spread: 100,
-  decay: 0.91,
-  scalar: 0.8,
-});
-
-fire(0.1, {
-  spread: 120,
-  startVelocity: 25,
-  decay: 0.92,
-  scalar: 1.2,
-});
-
-fire(0.1, {
-  spread: 120,
-  startVelocity: 45,
-});
-    }
-
-    else {
-        showOutput("No Profit No Loss", "blue");
-    }
+  else {
+    showOutput("No Profit No Loss", "blue");
+  }
 
 }
 
@@ -89,11 +89,11 @@ fire(0.1, {
 // calculateProfitAndLoss(10, 10, 10)
 
 
-function showOutput(message,color) {
-    outputBox.innerHTML = message;
-    outputBox.style.color = color 
+function showOutput(message, color) {
+  outputBox.innerHTML = message;
+  outputBox.style.color = color
 }
 
 function empty(msg) {
-    outputBox.innerHTML = msg;
+  outputBox.innerHTML = msg;
 }
